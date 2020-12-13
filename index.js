55393d23cc6f91cb61196c5e97bd2a908f680b6e
vasconevexd <vasco.neves98@gmail.com>
2020-12-13 01:08:59 +0000

    Server Created, User Creation Validations, Bcrypt & Jsonwebtoken

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+ },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB Connection established!");
+  }
+);
+
+app.use("/users", require("./routes/userRouter"));
---
+++ routes/userRouter.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+router.post("/register", async (req, res) => {
+    try{
+        let{email, password, passwordCheck, username} = req.body;
+        
+
+        if(!email || !password || !passwordCheck)
+            return res.status(400).json({msg: "Not all fields have been entered!"});
+        if(password.length < 5)
+            return res.status(400).json({msg: "The password needs to be at least 5 characters long."});
+        if(password !== passwordCheck)
+            return res.status(400).json({msg: "Enter the same password."});
+
+        const existingUser = await User.findOne({email: email});
+        
+        if(existingUser)
+            return res.status(400).json({msg: "An account with this email already exists."});
+        
+        if(!username) username = email;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password,salt);
+        
+        const newUser = new User({
+            email,
+            password: passwordHash,
+            username
+        });
+
+        const savedUser = await newUser.save();
+
+        res.json(savedUser);
+
+    } catch (err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.post("/login", async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+        
+        if(!email || !password)
+            return res.status(400).json({msg: "Not all fields have been entered!"});
+
+        const user = await User.findOne({email: email});
+        if(!user)
+            return res.status(400).json({msg: "No account with this email has been registered!"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg: "Invalid credentials."});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+
+    } catch (err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+module.exports = router;
